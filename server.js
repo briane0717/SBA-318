f3d962ef5dd83c58c41aab02bc90f5cdbf167315
Brian <briane0717@gmail.com>
2024-12-02 20:58:45 -0500

    created authorization middleware

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+function authenticate(req, res, next) {
+  if (!req.headers["authorization"]) {
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
+  next();
+}
+
+app.get("/dashboard", authenticate, (req, res) => {
+  res.send("Welcome to the Admin Dashboard");
+});
+
 app.get("/", (req, res) => {
   res.send("Front Page Headlines 📰");
 });
